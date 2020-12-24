be0a585da3b9a194dfd961a4c529df0443425a79
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-12-24 23:31:21 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+//
+// Created by Rostyslav Druzhchenko 24.12.20
+//
+
+#include <stdio.h>
+
+#include <rdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Called \"%s\" with %d arguments\n", argv[0], argc);
+    return 0;
+}
+
