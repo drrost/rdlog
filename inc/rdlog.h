0c1febef06c1656315a875037146dfc81304fa44
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-12-25 11:34:26 +0200

    Add sockets code

---
+++ inc/rdlog.h
@@ -0,0 +1,10 @@
+//
+// Created by Rostyslav Druzhchenko on 25.12.2020.
+//
+
+#ifndef TESTTHREADS_RDLOG_H
+#define TESTTHREADS_RDLOG_H
+
+void rd_validate(int argc, char *argv[]);
+
+#endif //TESTTHREADS_RDLOG_H
--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <rdlib.h>
 
+#include <rdlog.h>
+
 int n = 0;
 
 void *handle_message() {
@@ -33,12 +35,82 @@ void fun() {
     pthread_create(&ptid, NULL, &handle_message, NULL);
 }
 
-int main(int argc, char *argv[]) {
-    printf("Called \"%s\" with %d arguments\n", argv[0], argc);
+// ----
 
-    for (int i = 0; i < 10 ;i++)
-        fun();
-    sleep(1);
-    printf("Exiting main thread\n");
+typedef struct {
+    struct sockaddr_in address;
+    int fd;
+} t_socket_connection;
+
+t_socket_connection rd_open_socket_to_listen(int port) {
+    t_socket_connection connection;
+    struct sockaddr_in address;
+
+    connection.fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (connection.fd < 0) {
+        rd_printerr("socket() failed\n");
+        rd_printerr(strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    int res = bind(connection.fd, (struct sockaddr *)&address,
+                   sizeof(address));
+    if (res < 0) {
+        rd_printerr("bind() failed\n");
+        rd_printerr(strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(connection.fd, 3) < 0) {
+        rd_printerr("listen() failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return connection;
+}
+
+void rd_accept_from_socket(t_socket_connection connection) {
+    int socket = 0;
+    const int buff_size = 1024;
+    char *buffer = rd_strnew(buff_size);
+    int addr_size = sizeof(connection.address);
+
+    if ((socket = accept(connection.fd, (struct sockaddr *)&connection.address,
+                         (socklen_t *)&addr_size)) < 0) {
+        rd_printerr("accept() failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    read(socket, buffer, buff_size);
+    rd_log_d("LOGSRV", "Received log message: \"%s\"", buffer);
+
+    close(socket);
+}
+
+static void run_as_regular(int port) {
+    t_socket_connection connection = rd_open_socket_to_listen(port);
+
+//    rd_db_init();
+    rd_log_i("SRV: Start", "on port %d", port);
+
+    while (1) {
+//        rd_reset_leacks();
+        rd_accept_from_socket(connection);
+//        rd_check_leaks();
+    }
+}
+
+static void process(const char *port_str) {
+    int port = atoi(port_str);
+    run_as_regular(port);
+}
+
+int main(int argc, char *argv[]) {
+    rd_validate(argc, argv);
+    process(argv[1]);
     return 0;
 }
